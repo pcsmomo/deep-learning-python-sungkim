596e41db151b9f299cf73fdc3ba09ffb2847f022
Noah Kim <dongwoo_333@hanmail.net>
2022-12-28 07:38:28 +1100

    5. ML lab 02: TensorFlow - linear regression - 1

--- code/ml-lab-01/01-helloworld.py
+++ code/ml-lab-01/01-helloworld.py
@@ -1,9 +1,20 @@
 import tensorflow as tf
 
-print("TensorFlow version:", tf.__version__)
+# print("TensorFlow version:", tf.__version__)
 
-# Create a constant op
-# This op is added as a node to the default graph
-hello = tf.constant("Hellow, TensorFlow!")
+# # Create a constant op
+# # This op is added as a node to the default graph
+# hello = tf.constant("Hellow, TensorFlow!")
 
-tf.print(hello)
+# tf.print(hello)
+
+# Using session to follow the lecture
+# tf.compat.v1.disable_eager_execution()
+# tf.executing_eagerly()
+
+# https://github.com/OlafenwaMoses/ImageAI/issues/400
+# start a TF session
+with tf.compat.v1.Session() as sess:
+    hello = tf.constant("Hello, TensorFlow!")
+    # run the op and get result
+    print(sess.run(hello))
